c14c1869d2f44674a434491a39fcdc07d7732e7a
jzz1234 <jhaezerannedavid@gmail.com>
2025-08-02 22:46:17 +0800

    commit all

---
+++ script.js
@@ -0,0 +1,16 @@
+var num1 = 1;
+var num2 = 1;
+var total =0;
+
+if (addition){
+    total = num1 + num2;
+} 
+else if (subraction) {
+    total = num1 - num2;
+}
+else if (multiplication) {
+    total = num1  *num2;
+}
+    else if (divide) {
+    total = num1 / num2;
+}
