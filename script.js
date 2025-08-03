0529bf9c5f76ec8831bd204826120b53d4bba016
jzz1234 <jhaezerannedavid@gmail.com>
2025-08-03 23:28:15 +0800

    commit all

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
-var num1 = 1;
-var num2 = 1;
-var total =0;
+let transaction = '';
+let totalElement;
+let transactionElement;
 
-if (addition){
-    total = num1 + num2;
-} 
-else if (subraction) {
-    total = num1 - num2;
+window.onload = () => {
+    totalElement = document.querySelector('.total');
+    transactionElement = document.querySelector('.transaction');
+};
+
+function calculate(input) {
+    if (input === 'AC') {
+        transaction = '';
+        totalElement.style.display = 'none';
+        document.getElementById("mathTip").innerText = '';
+    } else if (input === '=') {
+        try {
+            let result = eval(transaction);
+            totalElement.innerText = result;
+            totalElement.style.display = 'block';
+
+            const tips = [
+                "Fun fact: bading k",
+                "Fun Fact: si bbm ay backburner ni sara",
+                "Fun fact: wag po ateh",
+                "Fun fact: teh barang ka te barang ka",
+                "Fun fact: dadih robbbb guYz mAy pinaPakitAh SiyaH SaH AkihN"
+            ];
+
+            let tip = tips[Math.floor(Math.random() * tips.length)];
+            alert(tip);
+
+        } catch {
+            totalElement.innerText = 'Error';
+            totalElement.style.display = 'block';
+        }
+    } else if (input === 'X') {
+        transaction = transaction.slice(0, -1);
+        totalElement.style.display = 'none';
+        document.getElementById("mathTip").innerText = '';
+    } else {
+        transaction += input;
+        totalElement.style.display = 'none';
+        document.getElementById("mathTip").innerText = '';
+    }
+
+    transactionElement.innerText = transaction;
 }
-else if (multiplication) {
-    total = num1  *num2;
-}
-    else if (divide) {
-    total = num1 / num2;
-}
+    
